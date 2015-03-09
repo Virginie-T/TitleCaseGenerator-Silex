bb8593451d15102d845820c44d9451214bc0c0c4
Tom Mertz & virginie trubiano <student@epicodus.com>
2015-03-09 13:55:21 -0700

    Add silex to titlecase

--- app/app.php
+++ app/app.php
@@ -5,11 +5,20 @@
 
 
     $app = new Silex\Application();
+    $app['debug'] = true;
 
     $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views'));
 
-    $app->get('/', function() use ($app)) {
+    $app->get('/', function() use ($app) {
         return $app['twig']->render('form.twig');
     });
+
+    $app->get("/view_title_case", function() use ($app)  {
+        $my_TitleCaseGenerator = new TitleCaseGenerator;
+        $title_cased_phrase = $my_TitleCaseGenerator->makeTitleCase($_GET['phrase']);
+        return $app['twig']->render('title_cased.twig', array('result' => $title_cased_phrase));
+    });
+
+    return $app;
 
  ?>
